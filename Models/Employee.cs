98a962c31f512ee37777644e89812dcea2aaf564
saud-ziz <s.hawas@barri.sa>
2025-02-26 19:33:17 +0300

    feat: enhance Employee model with validation attributes and default initialization

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,15 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace adeeb.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public DateTime JoinDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Department { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Position { get; set; }
 
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public DateTime? ExitDate { get; set; }
+
         // Navigation Property to SurveyResponse
-        public ICollection<SurveyResponse> SurveyResponses { get; set; }
+        public ICollection<SurveyResponse> SurveyResponses { get; set; } = new List<SurveyResponse>();
     }
 }
