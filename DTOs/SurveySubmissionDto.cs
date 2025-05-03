a94baa03ea9b380654002286ac8f09d46090fdc6
MohammadMakki-Dev <119872903+vxZeroKun@users.noreply.github.com>
2025-05-03 23:16:55 +0300

    change mistake in capitalization

--- DTOs/SurveySubmissionDto.cs
+++ DTOs/SurveySubmissionDto.cs
@@ -5,5 +5,5 @@ namespace AdeebBackend.DTOs;
 public class SurveySubmissionDto
 {
     public int? SurveyId { get; set; }
-    public List<QuestionSubmissionDto> QuestionsANswers { get; set; } = new List<QuestionSubmissionDto>();
+    public List<QuestionSubmissionDto> QuestionsAnswers { get; set; } = new List<QuestionSubmissionDto>();
 }
